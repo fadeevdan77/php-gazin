c638e8e14f162966d61af50a374026c0f0c2db7d
fadeevdan77 <97346734+fadeevdan77@users.noreply.github.com>
2022-01-10 19:35:29 +0500

    commit

--- 4/bd_fridge/gen_pdf.php
+++ 4/bd_fridge/gen_pdf.php
@@ -11,7 +11,7 @@
   $pdf -> AddFont("Arial", "", "arial.php");
   $pdf -> SetFont("Arial", "", "24");
 
-  $pdf -> Cell(300, 30, "<h1>Заявки на ремонт", 0, 1, "C");
+  $pdf -> Cell(100, 10, "Заявки на ремонт", 0, 1, "C");
 
   $pdf -> SetFont("Arial", "", "6");
 
